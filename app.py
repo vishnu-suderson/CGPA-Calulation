6881149e79077d0294bd233bb8616d200bf8bffb
vishnu_suderson <122167792+vishnu-suderson@users.noreply.github.com>
2025-06-27 14:03:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify,Response
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
